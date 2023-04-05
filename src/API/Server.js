ffe24bdb3b5915a29ca5884b4935b017fd2f1132
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-04-05 16:56:26 +0400

    URL2

--- src/API/Server.js
+++ src/API/Server.js
@@ -9,18 +9,18 @@ export class Server {
 
     // Получение всех сообщений где он получателя
     static async getMessageSrecipientId(recipientId) {
-        const response =  await axios.get(`https://task-6-server-70g2.onrender.com/api/message/recipientmessages?recipientId=${recipientId}`);
+        const response =  await axios.get(`https://task-4-client-rbhe.onrender.com/message/recipientmessages?recipientId=${recipientId}`);
         return response;
     }
 
     // Получение всех сообщений где он получателя
     static async getMessagesSenderId(recipientId) {
-        const response =  await axios.get(`https://task-6-server-70g2.onrender.com/api/message/sendermessages?senderId=${recipientId}`);
+        const response =  await axios.get(`https://task-4-client-rbhe.onrender.com/message/sendermessages?senderId=${recipientId}`);
         return response;
     }
     
     static async getMessage(senderid, recipientid, theme, body) {
-        const response = await axios.post('https://task-6-server-70g2.onrender.com/api/message/getmessage', { senderid, recipientid, theme, body });
+        const response = await axios.post('https://task-4-client-rbhe.onrender.com/message/getmessage', { senderid, recipientid, theme, body });
         return response;
     }
 
