d05890fc5607846ab2178e29fb0f2113d1494a33
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-04-05 19:10:00 +0400

    URL3

--- src/API/Server.js
+++ src/API/Server.js
@@ -3,24 +3,24 @@ export class Server {
 
     //Получение всех пользователей
     static async getAll() {
-        const response =  await axios.get('http://localhost:5000/api/user/users');
+        const response =  await axios.get('https://task-6-server-2pal.onrender.com/api/user/users');
         return response;
     }
 
     // Получение всех сообщений где он получателя
     static async getMessageSrecipientId(recipientId) {
-        const response =  await axios.get(`https://task-4-client-rbhe.onrender.com/message/recipientmessages?recipientId=${recipientId}`);
+        const response =  await axios.get(`https://task-6-server-2pal.onrender.com/message/recipientmessages?recipientId=${recipientId}`);
         return response;
     }
 
     // Получение всех сообщений где он получателя
     static async getMessagesSenderId(recipientId) {
-        const response =  await axios.get(`https://task-4-client-rbhe.onrender.com/message/sendermessages?senderId=${recipientId}`);
+        const response =  await axios.get(`https://task-6-server-2pal.onrender.com/message/sendermessages?senderId=${recipientId}`);
         return response;
     }
     
     static async getMessage(senderid, recipientid, theme, body) {
-        const response = await axios.post('https://task-4-client-rbhe.onrender.com/message/getmessage', { senderid, recipientid, theme, body });
+        const response = await axios.post('https://task-6-server-2pal.onrender.com/message/getmessage', { senderid, recipientid, theme, body });
         return response;
     }
 
